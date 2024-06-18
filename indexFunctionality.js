78bf89cc711083cd07eebc170a1649e995990b53
Daniel macharia <ndungudanny444@gmail.com>
2024-06-18 12:49:45 +0300

    re-structure porfolio
    merge components into the main page, remove individual component files.
    simplify portfolio

--- indexFunctionality.js
+++ indexFunctionality.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
       option.addEventListener('click', ()=>{
         //let val = selectedOption.style.dataset.optionValue;
         let val = option.value;
-        console.log(`clicked ${val}`);
+
         loadPage( getLinks()[val] );
         showMenuOptions(false);
       });
@@ -27,7 +27,11 @@ document.addEventListener('DOMContentLoaded', ()=>{
     console.log('body clicked');
   });
 
-  //loadFrames();
+  document.querySelectorAll('.footer-contact-item').forEach(contact=>{
+    contact.addEventListener('click', ()=>{
+      loadPage(getContact()[contact.dataset.destinationUrl]);
+    });
+  });
 });
 
 
@@ -79,15 +83,64 @@ function loadPage(url)
   anchor.remove();
 }
 
-function loadFrames()
-{
-  let frameNumber = 1;
-  document.querySelectorAll('iframe').forEach(frame=>{
-    let component = document.createElement('html');
-    component.href = 'resume.html';
 
-    frame.height = "" + (4 * component.height);
-
-    frameNumber++;
-  });
+function getContact(){
+return {
+  linkedIn: "https://www.linkedin.com/in/danny-ndung-u-46818a235?trk=contact-info",
+  email: "mailto:ndungudanny444@gmail.com",
+  github: "https://github.com/Daniel-Macharia"
+};
 }
+
+
+/* about functions */
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  let sectionCount = 0;
+  document.querySelectorAll('.about-sub-section').forEach( section=>{
+
+    if( sectionCount % 2 == 0 )
+    {
+      section.style.flexDirection = 'row-reverse';
+    }
+
+    sectionCount++;
+  });
+});
+
+
+/* resume functions */
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#resume-download-btn').addEventListener('click', () => {
+      console.log("Clicked donwload");
+      downloadResume();
+  });
+});
+
+function downloadResume()
+{
+    let resume = document.createElement('a');
+    //add a href to the resume url
+    resume.href = "images/resume.pdf";
+    //set the name for the downloaded resume
+    resume.download = "daniel-macharia-resume.pdf";
+
+    //set the anchor style so that it does not appear on the body
+    resume.style.display = "none";
+
+    //add the created anchor to the body of the html
+    document.querySelector('body').append(resume);
+
+    //trigger a click event on the added anchor so the resume downloads
+    resume.click();
+
+    //now that the resume has downloaded, remove it from the body
+    resume.remove();
+}
+
+//services function
+document.addEventListener('DOMContentLoaded', ()=>{
+  document.querySelector("#more-projects").addEventListener("click", ()=>{
+    loadPage("project.html");
+  });
+});
